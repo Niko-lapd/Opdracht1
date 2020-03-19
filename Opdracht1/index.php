ee01b894a2342996c8a13410700a60a5891f5b8e
Professional <2110699@talnet.nl>
2020-03-19 14:56:20 +0100

    Initial commit

---
+++ Opdracht1/index.php
@@ -0,0 +1,9 @@
+<?php
+$zin = "Er staat en aard in de gang";
+$waar = "bij juffrouw Jansen";
+$volledigeZin = "Er sttat een paard in de gang " . $waar;
+echo $volledigeZin;
+$a = " Ik ben niet geel eigenlijk";
+$b = "mijn vader is ook niet geel";
+$c = "mijn zus is wel geel tho";
+$volledigeZinnetje = $a . $b . $c;
